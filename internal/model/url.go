6186adcd0d8e839b1b76c271912e1dec34502ad0
thienphuc <thienphuc484@gmail.com>
2024-08-17 11:07:12 +0700

    Update model
    Delete id

--- internal/model/url.go
+++ internal/model/url.go
@@ -3,7 +3,6 @@ package model
 import "time"
 
 type URL struct {
-	Id        int64     `json:"id"`
 	Origin    string    `json:"origin"`
 	Code      string    `json:"code"`
 	CreatedAt time.Time `json:"created_at"`
