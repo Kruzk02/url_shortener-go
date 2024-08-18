aa345b3ee79c6f01869ae18fadacc1a8c577952b
thienphuc <thienphuc484@gmail.com>
2024-08-18 11:08:12 +0700

    Update db.go
    Add sync.Once to connect db once time

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -3,32 +3,37 @@ package repository
 import (
 	"database/sql"
 	"log"
+	"sync"
 
 	"github.com/go-sql-driver/mysql"
 )
 
-var db *sql.DB
+var (
+	db     *sql.DB
+	dbonce sync.Once
+)
 
 func InitDB() {
-	var err error
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "Password",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "url",
-	}
+	dbonce.Do(func() {
+		var err error
+		cfg := mysql.Config{
+			User:   "root",
+			Passwd: "Password",
+			Net:    "tcp",
+			Addr:   "127.0.0.1:3306",
+			DBName: "url",
+		}
 
-	db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatalf("Failed to open database: %v", err)
-	}
+		db, err = sql.Open("mysql", cfg.FormatDSN())
+		if err != nil {
+			log.Fatalf("Failed to open database: %v", err)
+		}
 
-	if err := db.Ping(); err != nil {
-		log.Fatalf("Failed to ping database: %v", err)
-	}
-
-	log.Println("Connected!")
+		if err := db.Ping(); err != nil {
+			log.Fatalf("Failed to ping database: %v", err)
+		}
+		log.Println("Database Connected!")
+	})
 }
 
 func GetDB() *sql.DB {
