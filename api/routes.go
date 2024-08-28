c498a51b51a9bf2f8dcad0dc81268c7b8aaaf960
thienphuc <thienphuc484@gmail.com>
2024-08-28 12:17:13 +0700

    Add CORS

--- api/routes.go
+++ api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net/http"
 	"url_shortener/internal/handler"
 
 	"github.com/gorilla/mux"
@@ -8,7 +9,22 @@ import (
 
 func SetupRoutes() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/url", handler.GetOriginByCodeHandler).Methods("GET")
-	r.HandleFunc("/url", handler.SaveUrlHandler).Methods("POST")
+	r.HandleFunc("/url", CORS(handler.GetOriginByCodeHandler)).Methods("GET", "OPTIONS")
+	r.HandleFunc("/url", CORS(handler.SaveUrlHandler)).Methods("POST", "OPTIONS")
 	return r
+}
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next(w, r)
+	}
 }
